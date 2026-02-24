fe3e7c15aae07d035608de49b5f3a37cdf688b6a
JuanCarlosHerediaJimenez <juancadev11@gmail.com>
2026-02-24 10:09:48 +0100

    FirstPush

---
+++ RPMI3D/Assets/_FPS_RPMI/Scripts/Player/FpController.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FpController : MonoBehaviour
+{
+    #region General Varialbes
+    [Header("Movement & Look")]
+    [SerializeField] GameObject camHolder;
+    [SerializeField] float speed = 5f;
+    [SerializeField] float sprintSpeed = 8f;
+    [SerializeField] float crouchSpeed = 3f;
+    [SerializeField] float maxForce = 1f;
+    [SerializeField] float sensibility = 0.1f;
+
+    [Header("Player State Bools")]
+    [SerializeField] bool isSprinting;
+    [SerializeField] bool isCrouching;
+
+    #endregion
+
+    //Variables de referencia privadas
+    Rigidbody rb;
+
+    //Variables para el input
+    Vector2 moveInput;
+    Vector2 lookInput;
+    float lookRotation;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Lock del cursor del raton
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+       
+    }
+
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+
+    }
+    #endregion
+}
